cb74c2ab9e9ea36f2d757ced9cec4a0cfc0a6388
DuZF@MacBook <du@mail.bnu.edu.cn>
2019-05-16 16:50:38 +0800

    No essential modifications

--- inc/file_o.h
+++ inc/file_o.h
@@ -8,6 +8,8 @@
 int make_directory(char * add_mkdir, char * label, char * scheme, char * version, int const m, int const n, int const SWITCH[], char * err_msg);
 
 
+int open_fruncate(char * err_msg, char * add, FILE ** fp);
+
 void matXwrite(int m, int n, double *data[], FILE * fp_write);
 
 
